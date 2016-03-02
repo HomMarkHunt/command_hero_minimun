00393a8e89e1a5f8558fe53bfffeb6350b975342
masateru homma <honmaso@gmail.com>
2016-03-02 20:20:23 +0900

    make while loop very simple case
    enndress loop still type "exit"

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -13,15 +13,18 @@ import java.io.InputStreamReader;
 public class Main {
 
 	public static void main(String[] args) {
-		boolean exit = true;
-		while (exit) {
+		while (true) {
 			try {
 				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
-				String line = br.readLine();
-				System.out.println(line);
+				String inputCommand = br.readLine();
+				if (inputCommand.equals("exit")) { 
+					break;
+				}
+				System.out.println(inputCommand);
 			} catch (IOException e) {
 				e.getMessage();
 			}
 		}
 	}
+	
 }
